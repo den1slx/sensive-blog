81376e362a99d8690973bf6414813c85307b1055
den1slx <keycoingold@mail.ru>
2023-05-14 02:00:34 +1000

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -88,12 +88,6 @@ class Post(models.Model):
         related_name='posts',
         verbose_name='Теги')
 
-    def __str__(self):
-        return self.title
-
-    def get_absolute_url(self):
-        return reverse('post_detail', args={'slug': self.slug})
-
     objects = PostQuerySet.as_manager()
 
     class Meta:
@@ -101,18 +95,15 @@ class Post(models.Model):
         verbose_name = 'пост'
         verbose_name_plural = 'посты'
 
-
-class Tag(models.Model):
-    title = models.CharField('Тег', max_length=20, unique=True)
-
     def __str__(self):
         return self.title
 
-    def clean(self):
-        self.title = self.title.lower()
-
     def get_absolute_url(self):
-        return reverse('tag_filter', args={'tag_title': self.slug})
+        return reverse('post_detail', args={'slug': self.slug})
+
+
+class Tag(models.Model):
+    title = models.CharField('Тег', max_length=20, unique=True)
 
     objects = TagQuerySet.as_manager()
 
@@ -120,6 +111,15 @@ class Tag(models.Model):
         ordering = ['title']
         verbose_name = 'тег'
         verbose_name_plural = 'теги'
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('tag_filter', args={'tag_title': self.slug})
+
+    def clean(self):
+        self.title = self.title.lower()
 
 
 class Comment(models.Model):
@@ -139,10 +139,10 @@ class Comment(models.Model):
     text = models.TextField('Текст комментария')
     published_at = models.DateTimeField('Дата и время публикации')
 
-    def __str__(self):
-        return f'{self.author.username} under {self.post.title}'
-
     class Meta:
         ordering = ['published_at']
         verbose_name = 'комментарий'
         verbose_name_plural = 'комментарии'
+
+    def __str__(self):
+        return f'{self.author.username} under {self.post.title}'
